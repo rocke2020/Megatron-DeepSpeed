3e0867030d29333b2cca935ded23b3a6b80d52b4
qichang.dong <qichang.dong@videt.cn>
2023-12-10 22:20:57 +0800

    u

---
+++ tools/run.sh
@@ -0,0 +1,12 @@
+# gpt2 preprocess_data
+file=tools/preprocess_data.py
+nohup python $file \
+    --input /mnt/nas1/dong-qichang/corpus/wiki/wikipedia/20230601/en_wikipedia.jsonl \
+    --output-prefix /mnt/nas1/dong-qichang/corpus/wiki/wikipedia/20230601/my-gpt2 \
+    --vocab-file /mnt/nas1/models/gpt2/vocab.json \
+    --dataset-impl mmap \
+    --tokenizer-type GPT2BPETokenizer \
+    --merge-file /mnt/nas1/models/gpt2/merges.txt \
+    --workers 24 \
+    --append-eod \
+    > $file.log 2>&1 &
